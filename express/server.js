684b16066fe6d9a3809bc0f8c743cd7ce7e53174
Kasilucky <kasimca44@gmail.com>
2020-01-03 20:50:52 +0530

    updated server.js

--- express/server.js
+++ express/server.js
@@ -4,14 +4,26 @@ const gql = require("graphql")
 const makeExecutableSchema = require("graphql-tools").makeExecutableSchema
 const cors = require("cors")
 const serverless = require("serverless-http")
+const path = require("path")
+const bodyParser = require("body-parser")
 
 const typeDefs = require("../graphql/typedefs")
 const resolvers = require("../graphql/resolvers")
 const app = express()
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
+const router = express.Router()
+router.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" })
+  res.write("<h1>Hello from Tweak Videos!</h1>")
+  res.end()
+})
+
+app.use(bodyParser.json())
+app.use("/.netlify/functions/server", router) // path must route to lambda
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")))
 app.use(
-  "/",
+  "/graphql",
   cors(),
   graphqlHTTP({
     schema,
--- server-local.js
+++ server-local.js
@@ -3,4 +3,4 @@ const port = process.env.PORT || 3003
 const app = require("./express/server.js")
 
 app.listen(port)
-console.log(`GraphiQL is now running on http://localhost:${port}/`)
+console.log(`GraphiQL is now running on http://localhost:${port}/graphql`)
